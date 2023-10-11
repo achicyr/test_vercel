af0556cab9481f8c4337f0f1f30fe69807da97ce
achicyr <hi.cyril@gmail.com>
2023-10-11 11:06:41 +0200

    ouioui

---
+++ _/backend/app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const { DB_USER, DB_PASSWORD } = process.env;
+app.get('/okok', (req, res, next) => {
+  res.send('oiuoioiyoiu')
+})
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,17 @@
+// Import packages
+const express = require("express");
+const home = require("./routes/home");
+
+// Middlewares
+const app = express();
+app.use(express.json());
+
+// Routes
+app.use("/home", home);
+app.get('/', (req,res) =>{
+    return res.status(200).end("oiuhiuhiuh");
+})
+
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
